6ed63c904167b1be8d8fee46da7a12f8d3f26cc6
ThiloMuller <thilo@prepaidmeters.com>
2021-12-28 16:01:51 +0200

    Add user registration Add user login Add user logout Add simple auth tokens

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+use Exception;
+
+class UserController extends Controller
+{
+    public function register(Request $request) {
+        
+        try {
+            $fields = $request->validate(
+                [
+                    'name' => 'required|string',
+                    'email' => 'required|string|unique:users,email',
+                    'password' => 'required|string|confirmed'
+                ]
+            );
+        } catch (exception $e) {
+            return response(['message'=>'Invalid User Criteria'], 400);
+        }
+
+        try {
+            $user = User::create([
+                'name' => $fields['name'],
+                'email' => $fields['email'],
+                'password' => Hash::make($fields['password'])
+            ]);
+        } catch (exception $e) {
+            return response(['message' => 'User could not be created'], 400);
+        };
+
+        return response(['user' => $user]);
+    }
+
+    public function login(Request $request) {
+        try {
+            $fields = $request->validate([
+                'email' => 'required|string',
+                'password' => 'required|string'
+            ]);
+        } catch(Exception $e) {
+            return response(['message' => $e->getMessage()], 400);
+        }
+
+        try{
+            $user = User::where('email', $fields['email'])->first();
+        } catch(Exception $e) {
+            return response(['message' => $e->getMessage()], 400);
+        }
+
+        if (!$user || !Hash::check($fields['password'], $user->password)) {
+            return response(['message' => 'Username or password does not match what we have on record'], 401);
+        }
+
+        $token = $user->createToken('cavemanDFSNyBXP')->plainTextToken;
+
+        return response(['user' => $user, 'token' => $token]);
+    }
+
+    public function logout(Request $request) {
+        auth()->user()->tokens()->delete();
+        return response(['message' => 'User Logged Out']);
+    }
+
+}
